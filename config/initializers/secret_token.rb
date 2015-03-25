8296986c83b809dbf98b22a53224bb99de887197
Anjali-patil <anjali.patil@anchanto.com>
2015-03-25 18:59:43 +0530

    footer UI

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeviAssignment::Application.config.secret_token = '29c4b31475c52ad84a9b1ec2caaaa6716d76e2ef65a94231fe2462c41694e51ef7e007bd8545afcc004ccca4b42c379fcc8d93c714923eb53790f709039bbea7'
